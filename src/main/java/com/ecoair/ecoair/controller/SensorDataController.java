534d2e4f8ae089a64302b914447af75c49f65239
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2025-10-04 16:37:04 -0300

    feat(controlle): todos os endpoits criados

--- src/main/java/com/ecoair/ecoair/controller/SensorDataController.java
+++ src/main/java/com/ecoair/ecoair/controller/SensorDataController.java
@@ -1,14 +1,14 @@
 package com.ecoair.ecoair.controller;
 
+import com.ecoair.ecoair.dtos.SensorDataRequestDTO;
 import com.ecoair.ecoair.dtos.SensorDataResponseDTO;
 import com.ecoair.ecoair.service.SensorDataService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -18,8 +18,65 @@ public class SensorDataController {
 
     private final SensorDataService sensorDataService;
 
+    @PostMapping
+    public ResponseEntity<SensorDataResponseDTO> createSensorData(@Valid @RequestBody SensorDataRequestDTO dto) {
+        SensorDataResponseDTO sensorCreate = sensorDataService.createSensorData(dto);
+        return ResponseEntity.status(201).body(sensorCreate);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Void> saveReading(@Valid @RequestBody SensorDataRequestDTO dto) {
+        sensorDataService.saveReading(dto);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SensorDataResponseDTO>> listSensorData() {
+        return ResponseEntity.ok(sensorDataService.listSensorData());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SensorDataResponseDTO> findSensorDataById(@PathVariable Long id) {
+        return ResponseEntity.ok(sensorDataService.findSensorDataById(id));
+    }
+
     @GetMapping("/{mac}")
     public ResponseEntity<List<SensorDataResponseDTO>> findSensorByMac(@PathVariable String mac) {
         return ResponseEntity.ok(sensorDataService.findSensorDataByMac(mac));
+    }
+
+    @GetMapping("/{mac}/range")
+    public ResponseEntity<List<SensorDataResponseDTO>> getByMacAndTimeRange(
+            @PathVariable String mac,
+            @RequestParam LocalDateTime startTime,
+            @RequestParam LocalDateTime endTime
+    ) {
+        var result = sensorDataService.findSensorDataByMacAndTimeRange(mac, startTime, endTime);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/gas")
+    public ResponseEntity<List<SensorDataResponseDTO>> getByGasType(
+            @RequestParam String gasType
+    ) {
+        var result = sensorDataService.findSensorDataByGasType(gasType);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/{mac}/latest")
+    public ResponseEntity<List<SensorDataResponseDTO>> getLatestByMac(
+            @PathVariable String mac
+    ) {
+        var result = sensorDataService.findLatestSensorDataByMac(mac);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/{mac}/gas")
+    public ResponseEntity<List<SensorDataResponseDTO>> getByMacAndGasType(
+            @PathVariable String mac,
+            @RequestParam String gasType
+    ) {
+        var result = sensorDataService.findSensorDataByMacAndGasType(mac, gasType);
+        return ResponseEntity.ok(result);
     }
 }
