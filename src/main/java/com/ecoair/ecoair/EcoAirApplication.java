b73e921592162ebd6b7c1dcc051b3d730ed5e33e
Leandro Ramos Cardoso <lrcardoso.jp@gmail.com>
2025-10-04 09:10:13 -0300

    first commit

---
+++ src/main/java/com/ecoair/ecoair/EcoAirApplication.java
@@ -0,0 +1,13 @@
+package com.ecoair.ecoair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoAirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoAirApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecoair/ecoair/EcoAirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoair.ecoair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoAirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
